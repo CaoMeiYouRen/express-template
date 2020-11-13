76659b6610d4dba0be4443fdc333836137e5c0f6
CaoMeiYouRen <996881204@qq.com>
2020-11-13 21:04:12 +0800

    feat: 优化 环境变量载入

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,21 @@
+import dotenv = require('dotenv')
+import path = require('path')
+import fs = require('fs-extra')
+const modes = [
+    '.env.local',
+    '.env',
+]
+let envParsed = {}
+for (let i = 0; i < modes.length; i++) {
+    const mode = modes[i]
+    const result = dotenv.config({ path: mode })
+    if (result.parsed) {
+        envParsed = Object.assign(result.parsed, envParsed)
+    }
+}
+if (process.env.NODE_ENV === 'development') {
+    console.log(envParsed)
+}
 const env = process.env
 export const PORT = Number(process.env.PORT || 5000)
 /**
